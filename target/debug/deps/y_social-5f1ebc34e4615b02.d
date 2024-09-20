9ba8986c9dd703205c8dac837bb13d1b8af08a9c
BackNot <alex.ashkov@proton.me>
2024-09-21 00:16:10 +0300

    removed compiled files"

--- target/debug/deps/y_social-5f1ebc34e4615b02.d
+++
@@ -1,5 +0,0 @@
-/Users/codific/Documents/workspace/y-social/target/debug/deps/liby_social-5f1ebc34e4615b02.rmeta: src/main.rs
-
-/Users/codific/Documents/workspace/y-social/target/debug/deps/y_social-5f1ebc34e4615b02.d: src/main.rs
-
-src/main.rs:
--- target/debug/y-social.d
+++
@@ -1,1 +0,0 @@
-/Users/codific/Documents/workspace/y-social/target/debug/y-social: /Users/codific/Documents/workspace/y-social/src/main.rs
